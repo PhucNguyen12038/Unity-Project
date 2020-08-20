32f269fbce0c48809b45b46053acedbeec8488d1
Nguyen Phuc <nguyenphuc@Nguyens-MacBook-Pro.local>
2020-08-20 17:35:57 +0300

    Unity Project

---
+++ Bowling/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    // Import all button of controller
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    // Start is called before the first frame update
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    void Start()
+    {
+        // Get the devices
+        List<InputDevice> devices = new List<InputDevice>();
+        // Get characteristices of controller like primary button, move, ... 
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if(devices.Count > 0)
+        {
+            // We can listen to many devices, choose to listen the first device 
+            targetDevice = devices[0];
+            // After getting devices, search a controller among the list
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.Log("Did not find the corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if (primaryButtonValue)
+            Debug.Log("Pressing Primary Button");
+
+        targetDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerValue);
+        if (triggerValue)
+            Debug.Log("Trigger pressed " + triggerValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        if (primary2DAxisValue != Vector2.zero)
+            Debug.Log("Primary Touchpad " + primary2DAxisValue);
+        */
+        if (showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+        }
+    }
+}
---
+++ Roll-a-ball/Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+    //public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    /*
+    void onMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    */
+
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+
+        }
+    }
+
+    private void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 3)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
